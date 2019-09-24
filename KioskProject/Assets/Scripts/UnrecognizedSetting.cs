4d445aad87e6e335ba6a0e763de43484f24bb5f0
JiyunYun <dbswldbs123@gmail.com>
2019-09-24 23:07:08 +0900

    1. solve android build problem 2. write UnrecognizedSetting script

---
+++ KioskProject/Assets/Scripts/UnrecognizedSetting.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class UnrecognizedSetting : MonoBehaviour, ITrackableEventHandler
+{
+    public TrackableBehaviour.Status status;
+    private bool isDetected = false;
+    private TrackableBehaviour mTrackableBehaviour;
+    private GameObject unrecognizedCanvas; // 이미지 타겟 미인식 시 나타나는 Canvas
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        unrecognizedCanvas = GameObject.Find("Unrecognized Canvas(screen space - overlay)");
+
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        status = newStatus;
+
+        if (newStatus == TrackableBehaviour.Status.DETECTED || newStatus == TrackableBehaviour.Status.TRACKED)
+        {
+            isDetected = true;
+        }
+        else
+        {
+            isDetected = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("status : " + status + " / isDetected : " + isDetected);
+
+        if (isDetected == true)
+        {
+            unrecognizedCanvas.SetActive(false);
+        }
+        else
+        {
+            unrecognizedCanvas.SetActive(true);
+        }
+    }
+}
