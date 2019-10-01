c74be51bfe078745bc2adeb5fd09e48c6a50a5cb
JiyunYun <dbswldbs123@gmail.com>
2019-10-01 15:11:57 +0900

    Complete TTS and STT for Software Contest Presentation

---
+++ KioskProject/Assets/STT/GoogleVoiceSpeech.cs
@@ -0,0 +1,208 @@
+﻿﻿//	Copyright (c) 2016 steele of lowkeysoft.com
+//        http://lowkeysoft.com
+//
+//	This software is provided 'as-is', without any express or implied warranty. In
+//	no event will the authors be held liable for any damages arising from the use
+//	of this software.
+//
+//	Permission is granted to anyone to use this software for any purpose,
+//	including commercial applications, and to alter it and redistribute it freely,
+//	subject to the following restrictions:
+//
+//	1. The origin of this software must not be misrepresented; you must not claim
+//	that you wrote the original software. If you use this software in a product,
+//	an acknowledgment in the product documentation would be appreciated but is not
+//	required.
+//
+//	2. Altered source versions must be plainly marked as such, and must not be
+//	misrepresented as being the original software.
+//
+//	3. This notice may not be removed or altered from any source distribution.
+//
+//  =============================================================================
+//
+// Acquired from https://github.com/steelejay/LowkeySpeech
+//
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Web;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent (typeof (AudioSource))]
+
+public class GoogleVoiceSpeech : MonoBehaviour {
+
+		public Text textBox;
+
+		struct ClipData
+		{
+            public int samples;
+		}
+
+		const int HEADER_SIZE = 44;
+
+		private int minFreq;
+		private int maxFreq;
+
+		private bool micConnected = false;
+
+		//A handle to the attached AudioSource
+		private AudioSource goAudioSource;
+
+		public string apiKey;
+
+	// Use this for initialization
+	void Start () {
+        textBox = GameObject.Find("Text_STT").GetComponent<Text>();
+
+		//Check if there is at least one microphone connected
+		if(Microphone.devices.Length <= 0)
+		{
+			//Throw a warning message at the console if there isn't
+			Debug.LogWarning("Microphone not connected!");
+		}
+		else //At least one microphone is present
+		{
+			//Set 'micConnected' to true
+			micConnected = true;
+
+			//Get the default microphone recording capabilities
+			Microphone.GetDeviceCaps(null, out minFreq, out maxFreq);
+
+			//According to the documentation, if minFreq and maxFreq are zero, the microphone supports any frequency...
+			if(minFreq == 0 && maxFreq == 0)
+			{
+					//...meaning 44100 Hz can be used as the recording sampling rate
+					maxFreq = 44100;
+			}
+
+			//Get the attached AudioSource component
+			goAudioSource = this.GetComponent<AudioSource>();
+		}
+	}
+
+	void OnGUI() 
+	{
+		//If there is a microphone
+		if(micConnected)
+		{
+			//If the audio from any microphone isn't being recorded
+			if(!Microphone.IsRecording(null))
+			{
+					//Case the 'Record' button gets pressed
+					if(GUI.Button(new Rect(Screen.width/2-100, Screen.height/2-25, 200, 50), "Record"))
+					{
+							//Start recording and store the audio captured from the microphone at the AudioClip in the AudioSource
+							goAudioSource.clip = Microphone.Start( null, true, 7, maxFreq); //Currently set for a 7 second clip
+					}
+			}
+			else //Recording is in progress
+			{
+								
+				//Case the 'Stop and Play' button gets pressed
+				if(GUI.Button(new Rect(Screen.width/2-100, Screen.height/2-25, 200, 50), "Stop and Play!"))
+				{
+					float filenameRand = UnityEngine.Random.Range (0.0f, 10.0f);
+
+					string filename = "testing" + filenameRand;
+
+					Microphone.End(null); //Stop the audio recording
+
+					Debug.Log("Recording Stopped");
+
+					if (!filename.ToLower().EndsWith(".wav")) {
+						filename += ".wav";
+					}
+
+					var filePath = Path.Combine("testing/", filename);
+					filePath = Path.Combine(Application.persistentDataPath, filePath);
+					Debug.Log("Created filepath string: " + filePath);
+
+					// Make sure directory exists if user is saving to sub dir.
+					Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+					SavWav.Save(filePath, goAudioSource.clip); //Save a temporary Wav File
+					Debug.Log("Saving @ " + filePath);
+					//Insert your API KEY here.
+					string apiURL = "https://speech.googleapis.com/v1/speech:recognize?&key=AIzaSyB0c4eMZ68z3CzAkjdZXlxiRg_db_y_FmY";
+					string Response;
+
+					Debug.Log("Uploading " + filePath);
+					Response = HttpUploadFile(apiURL, filePath, "file", "audio/wav; rate=44100");
+					Debug.Log("Response String: " + Response);
+
+					var jsonresponse = SimpleJSON.JSON.Parse(Response);
+
+					if (jsonresponse != null) {		
+						string resultString = jsonresponse["results"][0].ToString();
+						var jsonResults = SimpleJSON.JSON.Parse(resultString);
+
+                        string transcripts = jsonResults["alternatives"][0]["transcript"].ToString();
+
+						Debug.Log("transcript string: " + transcripts);
+						textBox.text = transcripts;
+					}
+
+					//goAudioSource.Play(); //Playback the recorded audio
+
+					File.Delete(filePath); //Delete the Temporary Wav file
+                }
+
+				GUI.Label(new Rect(Screen.width/2-100, Screen.height/2+25, 200, 50), "Recording in progress...");
+			}
+		}
+		else // No microphone
+		{
+			//Print a red "Microphone not connected!" message at the center of the screen
+			GUI.contentColor = Color.red;
+			GUI.Label(new Rect(Screen.width/2-100, Screen.height/2-25, 200, 50), "Microphone not connected!");
+		}
+	}
+
+    public string HttpUploadFile(string url, string file, string paramName, string contentType) {
+
+        System.Net.ServicePointManager.ServerCertificateValidationCallback += (o, certificate, chain, errors) => true;
+        Debug.Log(string.Format("Uploading {0} to {1}", file, url));
+
+        Byte[] bytes = File.ReadAllBytes(file);
+        String file64 = Convert.ToBase64String(bytes, Base64FormattingOptions.None);
+
+        Debug.Log(file64);
+
+        try
+        {
+            var httpWebRequest = (HttpWebRequest) WebRequest.Create(url);
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                string json = "{ \"config\" : { \"languageCode\" : \"ko-KR\" }, \"audio\" : { \"content\" : \"" + file64 + "\"} }";
+
+                Debug.Log(json);
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            Debug.Log(httpResponse);
+            
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+                Debug.Log("Response:" + result);
+                return result;
+            }
+
+        } catch (WebException ex) {
+            var resp = new StreamReader(ex.Response.GetResponseStream()).ReadToEnd();
+            Debug.Log(resp);
+        }
+        
+        return "empty";
+	}
+}
---
+++ KioskProject/Assets/STT/checkVoice.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class checkVoice : MonoBehaviour
+{
+    public Text stt;
+    public Text result;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stt = GameObject.Find("Text_STT").GetComponent<Text>();
+        result = GameObject.Find("Text_STT_Result").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void check(string text)
+    {
+        //string menu = "";
+
+        //switch (text)
+        //{
+        //    case "\"빅맥\"":
+        //        menu = "빅맥";
+        //        result.text = menu + " 주문을 안내해 드리겠습니다";
+        //        break;
+        //}
+    }
+}
